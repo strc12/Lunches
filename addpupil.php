23a373f84a7c13c0f041a45e7734a4eb8d3cdd71
strc12 <ric@oundleschool.org.uk>
2025-11-12 10:45:25 +0000

    fsadg

--- addpupil.php
+++ addpupil.php
@@ -1,20 +1,28 @@
 <?php
 print_r($_POST);
 include_once("connection.php");//import equivalent!
-$stmt=$conn->prepare("INSERT INTO tblusers 
-(UserID,Username,Surname,Forename,Password,Year,Balance,Role)
-VALUES
-(NULL,:Username,:Surname,:Forename,:Password,:Year,:Balance,:Role)
-");
-$stmt->bindParam(":Surname", $_POST["surname"]);
-$stmt->bindParam(":Forename", $_POST["forename"]);
-$stmt->bindParam(":Password", $_POST["password"]);
-$stmt->bindParam(":Year", $_POST["year"]);
-$stmt->bindParam(":Balance", $_POST["balance"]);
-$stmt->bindParam(":Role", $_POST["role"]);
-$stmt->bindParam(":Username", "bob");
+$role=1;
+$username="bob";
+try{
+    $stmt=$conn->prepare("INSERT INTO tblusers 
+    (UserID,Username,Surname,Forename,Password,Year,Balance,Role)
+    VALUES
+    (NULL,:Username,:Surname,:Forename,:Password,:Year,:Balance,:Role)
+    ");
+    $stmt->bindParam(":Surname", $_POST["surname"]);
+    $stmt->bindParam(":Forename", $_POST["forename"]);
+    $stmt->bindParam(":Password", $_POST["password"]);
+    $stmt->bindParam(":Year", $_POST["year"]);
+    $stmt->bindParam(":Balance", $_POST["balance"]);
+    $stmt->bindParam(":Role", $role);
+    $stmt->bindParam(":Username", $username);
+    $stmt->execute();
+}
+catch(PDOException $e)
+{
+    echo("error: " . $e->getMessage());
+}
 
 
-$stmt->execute();
 
 ?>
