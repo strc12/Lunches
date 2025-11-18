5ff9d2091467f7a9f0eb3ba63e451fc92b905a27
strc12 <ric@oundleschool.org.uk>
2025-11-18 11:21:08 +0000

    loginpage

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>          
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="processlogin.php" method="POST">
+        Username:<input type="text" name="surname"><br>
+        
+        Password:<input type="password" name="password"><br>
+        
+        <input type="submit" value="Login">
+    </form>
+    
+
+</body>
+</html>
